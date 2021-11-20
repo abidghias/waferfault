f31dc41f164949e1512908d4260ac5d545bca1d8
Mohamed Ghias <v-mghias@microsoft.com>
2021-11-20 18:41:47 +0530

    changes

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running with changes"
+    return "Flask app is running with changesfsf "
 
 port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
     host = '0.0.0.0'
-    app.run()
+    app.run(debug=True)
     httpd = simple_server.make_server(host=host, port=port, app=app)
     #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     print("Serving on %s %d" % (host, port))
